ba4823a7e5b13acd762dbe5c1d32dfb2273910c3
ylfyt <ayudi3521@gmail.com>
2023-01-25 21:18:55 +0700

    Initial Commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Router, Route } from 'svelte-navigator';
+	import Home from './pages/home.svelte';
+</script>
+
+<Router>
+	<main class="app">
+		<Route path="/*" primary={false}>
+			<Route path="/">
+				<Home />
+			</Route>
+		</Route>
+	</main>
+</Router>
+
+<style>
+</style>
